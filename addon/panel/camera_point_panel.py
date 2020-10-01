3992bf7ae18e4d5547fee936628e0d43ea02c6f7
Zageron <hello@adambryant.ca>
2020-09-30 23:50:40 -0700

    Add header and location property to the camera point panel

--- addon/panel/camera_point_panel.py
+++ addon/panel/camera_point_panel.py
@@ -1,5 +1,7 @@
 import bpy
 
+from bpy.props import FloatVectorProperty
+
 bl_info = {
     "name" : "Camera Point Panel",
     "author" : "Zageron",
@@ -25,8 +27,25 @@ class ZAG_CameraPointPanel(View3DPanel, bpy.types.Panel):
     bl_label = "Camera Point Panel"
     bl_context = "objectmode"
 
+    # Properties
+    location: FloatVectorProperty(
+        name="Location",
+        description="Location as a vector",
+        subtype="XYZ",
+        default=(0,0,0),
+        precision=6,
+        unit="LENGTH"
+    )
+
     def draw(self, context):
         layout = self.layout
+
+        selectedObject = bpy.context.view_layer.objects.active
+        id = selectedObject.get("uuid")
+        if id:
+            layout.label(text="Active Camera Point: " + selectedObject.name)
+            row = layout.row(align=True)
+            row.prop(selectedObject, "location")
 
         # Buttom to snap to the selected point (say we grabbed it in the collections)
         # Preview Mode vs Edit Mode
